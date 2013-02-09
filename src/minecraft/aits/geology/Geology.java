5296486a5ae59e81e74091c59fe7e4d23ca73662
Tristan Melton <charsmud@gmail.com>
2013-02-09 12:31:39 -0800

    Revert "Added new Cobblestones, Wall Tiles, Tiles, Anthracite Block, Bituminuous Coal block, peat block."
    This reverts commit 66e50a813d93e9590ad044ee72070662aec4e4eb.

--- src/minecraft/aits/geology/Geology.java
+++ src/minecraft/aits/geology/Geology.java
@@ -2,25 +2,13 @@ package aits.geology;
 
 import net.minecraft.block.Block;
 import net.minecraft.item.ItemStack;
-import net.minecraftforge.oredict.OreDictionary;
 import aits.api.IAITSModule;
-import aits.chemistry.blocks.BlockCarobiiteOre;
 import aits.core.DefaultProps;
 import aits.core.Version;
-import aits.core.regs.RegConfig;
-import aits.geology.blocks.BlockAnthracite;
-import aits.geology.blocks.BlockBituminousCoal;
 import aits.geology.blocks.BlockBricks;
-import aits.geology.blocks.BlockPeat;
-import aits.geology.blocks.BlockRockCobblestones;
-import aits.geology.blocks.BlockRockTiles;
 import aits.geology.blocks.BlockRocks;
-import aits.geology.blocks.BlockWallTiles;
 import aits.geology.items.ItemMultiBricks;
-import aits.geology.items.ItemMultiRockTiles;
 import aits.geology.items.ItemMultiRocks;
-import aits.geology.items.ItemRockCobblestones;
-import aits.geology.items.ItemRockWallTiles;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.common.registry.LanguageRegistry;
@@ -30,26 +18,11 @@ public class Geology implements IAITSModule
 {
 	public static Block stoneBrickMulti;
 	public static Block rockMulti;
-	public static Block tileMulti;
-	public static Block wallTileMulti;
-	public static Block rockCobblestones;
-	
-	public static Block blockPeat;
-	public static Block bituminousCoal;
-	public static Block blockAnthracite;
-	
 	@Override
 	public void loadBlocks() 
 	{
 		this.stoneBrickMulti = new BlockBricks(450);
 		this.rockMulti = new BlockRocks(451);
-		this.tileMulti = new BlockRockTiles(452);
-		this.wallTileMulti = new BlockWallTiles(453);
-		this.rockCobblestones = new BlockRockCobblestones(454);
-		
-		this.blockPeat = new BlockPeat(RegConfig.BlockIDRange + 30, 80).setBlockName("Peat");
-		this.bituminousCoal = new BlockBituminousCoal(RegConfig.BlockIDRange + 31, 81).setBlockName("Bituminous Coal Ore");
-		this.blockAnthracite = new BlockAnthracite(RegConfig.BlockIDRange + 32, 82).setBlockName("Anthracite Ore");
 	}
 
 	@Override
@@ -57,13 +30,6 @@ public class Geology implements IAITSModule
 	{
 		GameRegistry.registerBlock(this.stoneBrickMulti, ItemMultiBricks.class, "stoneBrickMulti");
 		GameRegistry.registerBlock(this.rockMulti, ItemMultiRocks.class, "rockMulti");
-		GameRegistry.registerBlock(this.tileMulti, ItemMultiRockTiles.class, "rockTiles");
-		GameRegistry.registerBlock(this.wallTileMulti, ItemRockWallTiles.class, "rockWallTiles");
-		GameRegistry.registerBlock(this.rockCobblestones, ItemRockCobblestones.class, "rockCobblestones");
-
-		GameRegistry.registerBlock(blockPeat, "blockPeat");
-		GameRegistry.registerBlock(blockAnthracite, "blockAnthracite");
-		GameRegistry.registerBlock(bituminousCoal, "bituminousCoal");
 	}
 
 	@Override
@@ -73,12 +39,9 @@ public class Geology implements IAITSModule
 	}
 
 	@Override
-	public void generateOreDictEntries() 
-	{
-
-		OreDictionary.registerOre("blockPeat", new ItemStack(blockPeat));
-		OreDictionary.registerOre("blockAnthracite", new ItemStack(blockAnthracite));
-		OreDictionary.registerOre("bituminousCoal", new ItemStack(bituminousCoal));
+	public void generateOreDictEntries() {
+		// TODO Auto-generated method stub
+		
 	}
 
 	@Override
@@ -103,7 +66,6 @@ public class Geology implements IAITSModule
 		LanguageRegistry.addName(new ItemStack(this.stoneBrickMulti, 1, 4), "Pink Granite Brick");
 		LanguageRegistry.addName(new ItemStack(this.stoneBrickMulti, 1, 5), "Marble Brick");
 		LanguageRegistry.addName(new ItemStack(this.stoneBrickMulti, 1, 6), "Quartzite Brick");
-		
 		LanguageRegistry.addName(new ItemStack(this.rockMulti, 1, 0), "Slate");
 		LanguageRegistry.addName(new ItemStack(this.rockMulti, 1, 1), "Limestone");
 		LanguageRegistry.addName(new ItemStack(this.rockMulti, 1, 2), "Basalt");
@@ -111,37 +73,5 @@ public class Geology implements IAITSModule
 		LanguageRegistry.addName(new ItemStack(this.rockMulti, 1, 4), "Pink Granite");
 		LanguageRegistry.addName(new ItemStack(this.rockMulti, 1, 5), "Marble");
 		LanguageRegistry.addName(new ItemStack(this.rockMulti, 1, 6), "Quartzite");
-	
-		LanguageRegistry.addName(new ItemStack(this.tileMulti, 1, 0), "Slate Tile");
-		LanguageRegistry.addName(new ItemStack(this.tileMulti, 1, 1), "Limestone Tile");
-		LanguageRegistry.addName(new ItemStack(this.tileMulti, 1, 2), "Basalt Tile");
-		LanguageRegistry.addName(new ItemStack(this.tileMulti, 1, 3), "Granite Tile");
-		LanguageRegistry.addName(new ItemStack(this.tileMulti, 1, 4), "Pink Granite Tile");
-		LanguageRegistry.addName(new ItemStack(this.tileMulti, 1, 5), "Marble Tile");
-		LanguageRegistry.addName(new ItemStack(this.tileMulti, 1, 6), "Quartzite Tile");
-
-		LanguageRegistry.addName(new ItemStack(this.wallTileMulti, 1, 0), "Slate Wall Tile");
-		LanguageRegistry.addName(new ItemStack(this.wallTileMulti, 1, 1), "Limestone Wall Tile");
-		LanguageRegistry.addName(new ItemStack(this.wallTileMulti, 1, 2), "Basalt Wall Tile");
-		LanguageRegistry.addName(new ItemStack(this.wallTileMulti, 1, 3), "Granite Wall Tile");
-		LanguageRegistry.addName(new ItemStack(this.wallTileMulti, 1, 4), "Pink Granite Wall Tile");
-		LanguageRegistry.addName(new ItemStack(this.wallTileMulti, 1, 5), "Marble Wall Tile");
-		LanguageRegistry.addName(new ItemStack(this.wallTileMulti, 1, 6), "Quartzite Wall Tile");
-
-		LanguageRegistry.addName(new ItemStack(this.rockCobblestones, 1, 0), "Slate Cobblestone");
-		LanguageRegistry.addName(new ItemStack(this.rockCobblestones, 1, 1), "Limestone Cobblestone");
-		LanguageRegistry.addName(new ItemStack(this.rockCobblestones, 1, 2), "Basalt Cobblestone");
-		LanguageRegistry.addName(new ItemStack(this.rockCobblestones, 1, 3), "Granite Cobblestone");
-		LanguageRegistry.addName(new ItemStack(this.rockCobblestones, 1, 4), "Pink Granite Cobblestone");
-		LanguageRegistry.addName(new ItemStack(this.rockCobblestones, 1, 5), "Marble Cobblestone");
-		LanguageRegistry.addName(new ItemStack(this.rockCobblestones, 1, 6), "Quartzite Cobblestone");
-
-		LanguageRegistry.addName(bituminousCoal, "Bituminous Coal Ore");
-		LanguageRegistry.addName(blockPeat, "Peat");
-		LanguageRegistry.addName(blockAnthracite, "Anthracite Ore");
-
-		
-
-
 	}
 }
